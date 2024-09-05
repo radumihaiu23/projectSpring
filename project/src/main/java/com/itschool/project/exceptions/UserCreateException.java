8d02b3c88bcfa0bfcc97393bcb12a85834f93860
raduM <23radumihaiu@gmail.com>
2024-09-05 21:43:02 +0300

    Session 28 - Exceptions - add

---
+++ project/src/main/java/com/itschool/project/exceptions/UserCreateException.java
@@ -0,0 +1,9 @@
+package com.itschool.project.exceptions;
+
+public class UserCreateException extends RuntimeException {
+
+    public UserCreateException(String message) {
+        super(message);
+    }
+
+}
